1dda728d4613826771e23a71947175a24bd76a6d
Ryzen9-5950X-RTX3090 <jordanluders@gmail.com>
2022-11-25 10:22:14 -0600

    Upload.

---
+++ My Summer Game 2/Assets/Scripts/Home.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour {
+
+	// Use this for initialization
+	public void Click () {
+		SceneManager.LoadScene ("Home");
+	}
+
+	// Update is called once per frame
+
+}
