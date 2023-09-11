4bd72cabe02c6007adda4b58dd40859d6ebafe7a
Ryzen5-3600 <jordanluders@proton.me>
2023-09-10 19:15:15 -0500

    Merge code from: https://github.com/R5-3600/FlappyTux-UnityProject

---
+++ Flappy Tux/Assets/Scripts/Game_lightmode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_lightmode : MonoBehaviour {
+
+	// Use this for initialization
+	public void Click () {
+		SceneManager.LoadScene ("Game_light-mode");
+	}
+
+	// Update is called once per frame
+
+}
---
+++ Flappy Tux/Assets/Scripts/MainMenu_lightmode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_lightmode : MonoBehaviour {
+
+	// Use this for initialization
+	public void Click () {
+		SceneManager.LoadScene ("MainMenu_light-mode");
+	}
+
+	// Update is called once per frame
+
+}
